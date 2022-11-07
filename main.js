590b9d5f0362b1201ad50679d9eb004e70b87b1b
Wayne <waynegabriel13@gmail.com>
2022-11-06 21:01:16 -0300

    final touches

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 function createGame(player1, hour, player2) {
   return `
-<li>
-    <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
-    <strong>${hour}</strong>
-    <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
-</li>
+  <li>
+      <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+      <strong>${hour}</strong>
+      <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+  </li>
 `
 }
 
+
+let delay = -0.2
 function createCard(date, day, game) {
+  delay += 0.2
   return `
-  <div class="card">
+  <div class="card" style="animation-delay: ${delay}s">
         <h2>${date} <span>${day}</span></h2>
         <ul>
             ${game}
@@ -19,34 +22,27 @@ function createCard(date, day, game) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-<header>
-  <img src="./assets/logo.svg" alt="Logo da NLW">
-  </header>
-  <main id="cards">
-      ${createCard("20/11", "domingo", createGame('qatar', '13:00', 'serbia'))}
+document.querySelector("#cards").innerHTML =
 
-      ${createCard("21/11", "segunda", 
-          createGame('england', '10:00', 'iran') +
-          createGame('senegal', '13:00', 'netherlands') +
-          createGame('usa', '16:00', 'wales')
-      )}
+  createCard("20/11", "domingo",
+    createGame('qatar', '13:00', 'serbia'))
+  +
 
+  createCard("21/11", "segunda",
+    createGame('england', '10:00', 'iran') +
+    createGame('senegal', '13:00', 'netherlands') +
+    createGame('usa', '16:00', 'wales'))
+  +
 
-      ${createCard("22/11", "terça", 
-          createGame('argentina', '07:00', 'arabia') +
-          createGame('denmark', '10:00', 'tunisia') +
-          createGame('mexico', '13:00', 'poland') +
-          createGame('france', '16:00', 'australia')
-          )}
+  createCard("22/11", "terça",
+    createGame('argentina', '07:00', 'arabia') +
+    createGame('denmark', '10:00', 'tunisia') +
+    createGame('mexico', '13:00', 'poland') +
+    createGame('france', '16:00', 'australia'))
+  +
 
-      ${createCard("23/11", "quarta",
-            createGame('marocco', '07:00', 'croatia') +
-            createGame('germany', '10:00', 'japan') +
-            createGame('spain', '13:00', 'costarica') +
-            createGame('belgium', '16:00', 'canada')
-          )}
-
-      
-  </main>
-`
+  createCard("23/11", "quarta",
+    createGame('marocco', '07:00', 'croatia') +
+    createGame('germany', '10:00', 'japan') +
+    createGame('spain', '13:00', 'costarica') +
+    createGame('belgium', '16:00', 'canada'))
